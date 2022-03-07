25d52651d419082efb0f8d9cfb73dc048d9678fa
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-03-07 23:07:05 +0700

    Custom SplashScreen

--- app/src/main/java/com/ahmadabuhasan/pointofsale/SplashScreenActivity.java
+++ app/src/main/java/com/ahmadabuhasan/pointofsale/SplashScreenActivity.java
@@ -1,11 +1,11 @@
 package com.ahmadabuhasan.pointofsale;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.credentials.CredentialsApi;
 
