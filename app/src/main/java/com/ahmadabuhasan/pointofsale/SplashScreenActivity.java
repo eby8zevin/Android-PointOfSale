a4d422b735df955fc0196f6ff572aabe2ff9ae04
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-03-12 06:17:24 +0700

    set splashTimeOut + Layout Full Screen

--- app/src/main/java/com/ahmadabuhasan/pointofsale/SplashScreenActivity.java
+++ app/src/main/java/com/ahmadabuhasan/pointofsale/SplashScreenActivity.java
@@ -1,11 +1,13 @@
 package com.ahmadabuhasan.pointofsale;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.credentials.CredentialsApi;
 
@@ -14,11 +16,13 @@ import java.util.Objects;
 @SuppressLint("CustomSplashScreen")
 public class SplashScreenActivity extends AppCompatActivity {
 
-    public static int splashTimeOut = CredentialsApi.CREDENTIAL_PICKER_REQUEST_CODE;
+    public static int splashTimeOut = CredentialsApi.CREDENTIAL_PICKER_REQUEST_CODE + 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
         Objects.requireNonNull(getSupportActionBar()).hide();
 
