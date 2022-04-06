777f4cc28703b1eaef66e031303208b3163fd257
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-04-07 00:22:54 +0700

    Dashboard

--- app/src/main/java/com/ahmadabuhasan/pointofsale/DashboardActivity.java
+++ app/src/main/java/com/ahmadabuhasan/pointofsale/DashboardActivity.java
@@ -1,14 +1,28 @@
 package com.ahmadabuhasan.pointofsale;
 
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.ahmadabuhasan.pointofsale.databinding.ActivityDashboardBinding;
+
+import java.util.Objects;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private ActivityDashboardBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        binding = ActivityDashboardBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        binding.textAppName.setSelected(true);
     }
 }
