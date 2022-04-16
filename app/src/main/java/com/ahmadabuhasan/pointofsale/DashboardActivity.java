a80271eff95ab68d2cabe251e33a5724afd1c0c8
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-04-16 23:40:28 +0700

    Working...

--- app/src/main/java/com/ahmadabuhasan/pointofsale/DashboardActivity.java
+++ app/src/main/java/com/ahmadabuhasan/pointofsale/DashboardActivity.java
@@ -1,9 +1,13 @@
 package com.ahmadabuhasan.pointofsale;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.ahmadabuhasan.pointofsale.databinding.ActivityDashboardBinding;
@@ -24,5 +28,29 @@ public class DashboardActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).hide();
 
         binding.textAppName.setSelected(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.language_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.local_english:
+                Toast.makeText(getApplicationContext(), "English", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.local_indonesian:
+                Toast.makeText(getApplicationContext(), "Indonesian", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void setNewLocale(AppCompatActivity appCompatActivity, String language) {
+
     }
 }
