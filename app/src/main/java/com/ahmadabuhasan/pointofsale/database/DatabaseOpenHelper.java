1425732e5a490cb02eb89afb05eb7f36ee0c154b
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-05-02 13:58:29 +0700

    Database Open Helper

---
+++ app/src/main/java/com/ahmadabuhasan/pointofsale/database/DatabaseOpenHelper.java
@@ -0,0 +1,85 @@
+package com.ahmadabuhasan.pointofsale.database;
+
+import android.content.Context;
+
+import com.ahmadabuhasan.pointofsale.R;
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+import es.dmoral.toasty.Toasty;
+
+public class DatabaseOpenHelper extends SQLiteAssetHelper {
+
+    public static final String DATABASE_NAME = "POS.db";
+    private static final int DATABASE_VERSION = 1;
+    private Context context;
+
+    private DatabaseOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+        this.context = context;
+    }
+
+    public void backup(String outFileName) {
+
+        //database path
+        final String inFileName = context.getDatabasePath(DATABASE_NAME).toString();
+
+        try {
+            File dbFile = new File(inFileName);
+            FileInputStream fis = new FileInputStream(dbFile);
+
+            // Open the empty db as the output stream
+            OutputStream output = new FileOutputStream(outFileName);
+
+            //
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = fis.read(buffer)) > 0) {
+                output.write(buffer, 0, length);
+            }
+            // Close the streams
+            output.flush();
+            output.close();
+            fis.close();
+
+            Toasty.success(context, R.string.backup_completed_successfully, Toasty.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toasty.error(context, R.string.unable_to_backup_database_retry, Toasty.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
+    public void importDB(String inFileName) {
+
+        final String outFileName = context.getDatabasePath(DATABASE_NAME).toString();
+
+        try {
+            File dbFile = new File(inFileName);
+            FileInputStream fis = new FileInputStream(dbFile);
+
+            // Open the empty db as the output stream
+            OutputStream output = new FileOutputStream(outFileName);
+
+            // Transfer bytes from the input file to the output file
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = fis.read(buffer)) > 0) {
+                output.write(buffer, 0, length);
+            }
+
+            // Close the streams
+            output.flush();
+            output.close();
+            fis.close();
+
+            Toasty.success(context, R.string.database_Import_completed, Toasty.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toasty.error(context, R.string.unable_to_import_database_retry, Toasty.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+}
