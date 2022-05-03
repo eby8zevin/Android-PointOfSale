6eda5c9c9ba2ca3fe64166e5b7af427ee98ba1e5
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-05-03 16:24:17 +0700

    binding RecyclerView

--- app/src/main/java/com/ahmadabuhasan/pointofsale/product/ProductActivity.java
+++ app/src/main/java/com/ahmadabuhasan/pointofsale/product/ProductActivity.java
@@ -1,16 +1,54 @@
 package com.ahmadabuhasan.pointofsale.product;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.ahmadabuhasan.pointofsale.R;
+import com.ahmadabuhasan.pointofsale.database.DatabaseAccess;
+import com.ahmadabuhasan.pointofsale.databinding.ActivityDashboardBinding;
+import com.ahmadabuhasan.pointofsale.databinding.ActivityProductBinding;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
+import es.dmoral.toasty.Toasty;
 
 public class ProductActivity extends AppCompatActivity {
+
+    private ActivityProductBinding binding;
+    ProductAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product);
+        binding = ActivityProductBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Objects.requireNonNull(getSupportActionBar()).setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.all_product);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        binding.productRecyclerview.setLayoutManager(layoutManager);
+        binding.productRecyclerview.setHasFixedSize(true);
+
+        DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
+        databaseAccess.open();
+        List<HashMap<String, String>> productData = databaseAccess.getProducts();
+        Log.d("data", "" + productData.size());
+        if (productData.size() <= 0) {
+            Toasty.info(this, R.string.no_product_found, Toasty.LENGTH_SHORT).show();
+            this.binding.ivNoProduct.setImageResource(R.drawable.no_data);
+        } else {
+            this.binding.ivNoProduct.setVisibility(View.GONE);
+            this.binding.productRecyclerview.setAdapter(adapter);
+        }
+
     }
 }
