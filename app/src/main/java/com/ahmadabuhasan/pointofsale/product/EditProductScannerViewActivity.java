bf1e6f897cc1ae36de3f28e95a1c358709c5e929
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-05-04 12:22:08 +0700

    QR or Barcode Scanner

---
+++ app/src/main/java/com/ahmadabuhasan/pointofsale/product/EditProductScannerViewActivity.java
@@ -0,0 +1,16 @@
+package com.ahmadabuhasan.pointofsale.product;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ahmadabuhasan.pointofsale.R;
+
+public class EditProductScannerViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_product_scanner_view);
+    }
+}
