573060c22e3bd8444e51de402900887ea702cfa2
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-05-04 12:24:04 +0700

    Add Product Activity

--- app/src/main/java/com/ahmadabuhasan/pointofsale/product/AddProductActivity.java
+++ app/src/main/java/com/ahmadabuhasan/pointofsale/product/AddProductActivity.java
@@ -2,15 +2,34 @@ package com.ahmadabuhasan.pointofsale.product;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.ahmadabuhasan.pointofsale.R;
+import com.ahmadabuhasan.pointofsale.databinding.ActivityAddProductBinding;
+
+import java.util.Objects;
 
 public class AddProductActivity extends AppCompatActivity {
+
+    public static EditText etProductCode;
+    private ActivityAddProductBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_product);
+        binding = ActivityAddProductBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Objects.requireNonNull(getSupportActionBar()).setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.add_product);
+
+        etProductCode = binding.etProductCode;
+
+        this.binding.ivScanCode.setOnClickListener(view -> AddProductActivity.this.startActivity(new Intent(AddProductActivity.this, ScannerViewActivity.class)));
+
     }
 }
