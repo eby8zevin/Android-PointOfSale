7341c807de9e40a2b002019eefbb62c19cd48e4f
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-05-04 07:16:39 +0700

    Working. . .

---
+++ app/src/main/java/com/ahmadabuhasan/pointofsale/product/AddProductActivity.java
@@ -0,0 +1,16 @@
+package com.ahmadabuhasan.pointofsale.product;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ahmadabuhasan.pointofsale.R;
+
+public class AddProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_product);
+    }
+}
