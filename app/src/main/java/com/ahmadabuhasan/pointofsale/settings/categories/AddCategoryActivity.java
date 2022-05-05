70fc3cca7acbee73c73dba7218f94100d4f4fc68
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-05-05 19:46:41 +0700

    Add Category Activity

--- app/src/main/java/com/ahmadabuhasan/pointofsale/settings/categories/AddCategoryActivity.java
+++ app/src/main/java/com/ahmadabuhasan/pointofsale/settings/categories/AddCategoryActivity.java
@@ -1,16 +1,64 @@
 package com.ahmadabuhasan.pointofsale.settings.categories;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 
 import com.ahmadabuhasan.pointofsale.R;
+import com.ahmadabuhasan.pointofsale.database.DatabaseAccess;
+import com.ahmadabuhasan.pointofsale.databinding.ActivityAddCategoryBinding;
+import com.ahmadabuhasan.pointofsale.utils.BaseActivity;
+//import com.itextpdf.text.io.PagedChannelRandomAccessSource;
 
-public class AddCategoryActivity extends AppCompatActivity {
+import java.util.Objects;
+
+import es.dmoral.toasty.Toasty;
+
+public class AddCategoryActivity extends BaseActivity {
+
+    private ActivityAddCategoryBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_category);
+        binding = ActivityAddCategoryBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Objects.requireNonNull(getSupportActionBar()).setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.add_category);
+
+        this.binding.tvAddCategory.setOnClickListener(view -> {
+            String category_name = AddCategoryActivity.this.binding.etCategoryName.toString().trim();
+            if (category_name.isEmpty()) {
+                AddCategoryActivity.this.binding.etCategoryName.setError(AddCategoryActivity.this.getString(R.string.enter_category_name));
+                AddCategoryActivity.this.binding.etCategoryName.requestFocus();
+                return;
+            }
+
+            DatabaseAccess databaseAccess = DatabaseAccess.getInstance(AddCategoryActivity.this);
+            databaseAccess.open();
+
+            if (databaseAccess.addCategory(category_name)) {
+                Toasty.success(AddCategoryActivity.this, R.string.category_added_successfully, Toasty.LENGTH_SHORT).show();
+                Intent i = new Intent(AddCategoryActivity.this, CategoriesActivity.class);
+                //i.addFlags(PagedChannelRandomAccessSource.DEFAULT_TOTAL_BUFSIZE);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                AddCategoryActivity.this.startActivity(i);
+                return;
+            }
+            Toasty.error(AddCategoryActivity.this, R.string.failed, Toasty.LENGTH_SHORT).show();
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() != android.R.id.home) {
+            return super.onOptionsItemSelected(item);
+        }
+        finish();
+        return true;
     }
 }
