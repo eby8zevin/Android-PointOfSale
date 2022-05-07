caea9d1066f073efeecff39c1a8e2d90eb32dd34
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-05-07 21:28:34 +0700

    Add Weight Activity

--- app/src/main/java/com/ahmadabuhasan/pointofsale/settings/weight/AddWeightActivity.java
+++ app/src/main/java/com/ahmadabuhasan/pointofsale/settings/weight/AddWeightActivity.java
@@ -1,16 +1,63 @@
 package com.ahmadabuhasan.pointofsale.settings.weight;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 
 import com.ahmadabuhasan.pointofsale.R;
+import com.ahmadabuhasan.pointofsale.database.DatabaseAccess;
+import com.ahmadabuhasan.pointofsale.databinding.ActivityAddWeightBinding;
+import com.ahmadabuhasan.pointofsale.utils.BaseActivity;
+//import com.itextpdf.text.io.PagedChannelRandomAccessSource;
 
-public class AddWeightActivity extends AppCompatActivity {
+import java.util.Objects;
+
+import es.dmoral.toasty.Toasty;
+
+public class AddWeightActivity extends BaseActivity {
+
+    private ActivityAddWeightBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_weight);
+        binding = ActivityAddWeightBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Objects.requireNonNull(getSupportActionBar()).setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.add_weight);
+
+        this.binding.tvAddWeight.setOnClickListener(view -> {
+            String Weight_name = AddWeightActivity.this.binding.etWeUnNameName.getText().toString().trim();
+            if (Weight_name.isEmpty()) {
+                AddWeightActivity.this.binding.etWeUnNameName.setError(AddWeightActivity.this.getString(R.string.enter_weight_name));
+                AddWeightActivity.this.binding.etWeUnNameName.requestFocus();
+                return;
+            }
+
+            DatabaseAccess databaseAccess = DatabaseAccess.getInstance(AddWeightActivity.this);
+            databaseAccess.open();
+
+            if (databaseAccess.addWeight(Weight_name)) {
+                Toasty.success(AddWeightActivity.this, R.string.weight_added_successfully, Toasty.LENGTH_SHORT).show();
+                Intent i = new Intent(AddWeightActivity.this, WeightActivity.class);
+                //i.addFlags(PagedChannelRandomAccessSource.DEFAULT_TOTAL_BUFSIZE);
+                AddWeightActivity.this.startActivity(i);
+                return;
+            }
+            Toasty.error(AddWeightActivity.this, R.string.failed, Toasty.LENGTH_SHORT).show();
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() != android.R.id.home) {
+            return super.onOptionsItemSelected(item);
+        }
+        finish();
+        return true;
     }
 }
