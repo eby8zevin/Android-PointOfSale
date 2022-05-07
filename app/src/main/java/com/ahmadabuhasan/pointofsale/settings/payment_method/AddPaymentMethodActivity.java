f758e921cd56cd32c8b8595c1c798bca84a6a573
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-05-07 19:17:26 +0700

    Add Payment Method Activity

--- app/src/main/java/com/ahmadabuhasan/pointofsale/settings/payment_method/AddPaymentMethodActivity.java
+++ app/src/main/java/com/ahmadabuhasan/pointofsale/settings/payment_method/AddPaymentMethodActivity.java
@@ -1,16 +1,63 @@
 package com.ahmadabuhasan.pointofsale.settings.payment_method;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 
 import com.ahmadabuhasan.pointofsale.R;
+import com.ahmadabuhasan.pointofsale.database.DatabaseAccess;
+import com.ahmadabuhasan.pointofsale.databinding.ActivityAddPaymentMethodBinding;
+import com.ahmadabuhasan.pointofsale.utils.BaseActivity;
+//import com.itextpdf.text.io.PagedChannelRandomAccessSource;
 
-public class AddPaymentMethodActivity extends AppCompatActivity {
+import java.util.Objects;
+
+import es.dmoral.toasty.Toasty;
+
+public class AddPaymentMethodActivity extends BaseActivity {
+
+    private ActivityAddPaymentMethodBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_payment_method);
+        binding = ActivityAddPaymentMethodBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Objects.requireNonNull(getSupportActionBar()).setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.add_payment_method);
+
+        this.binding.tvAddPaymentMethod.setOnClickListener(view -> {
+            String payment_method_name = AddPaymentMethodActivity.this.binding.etPaymentMethodName.getText().toString().trim();
+            if (payment_method_name.isEmpty()) {
+                AddPaymentMethodActivity.this.binding.etPaymentMethodName.setError(AddPaymentMethodActivity.this.getString(R.string.enter_payment_method_name));
+                AddPaymentMethodActivity.this.binding.etPaymentMethodName.requestFocus();
+                return;
+            }
+
+            DatabaseAccess databaseAccess = DatabaseAccess.getInstance(AddPaymentMethodActivity.this);
+            databaseAccess.open();
+
+            if (databaseAccess.addPaymentMethod(payment_method_name)) {
+                Toasty.success(AddPaymentMethodActivity.this, R.string.successfully_added, Toasty.LENGTH_SHORT).show();
+                Intent i = new Intent(AddPaymentMethodActivity.this, PaymentMethodActivity.class);
+                //i.addFlags(PagedChannelRandomAccessSource.DEFAULT_TOTAL_BUFSIZE);
+                AddPaymentMethodActivity.this.startActivity(i);
+                return;
+            }
+            Toasty.error(AddPaymentMethodActivity.this, R.string.failed, Toasty.LENGTH_SHORT).show();
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() != android.R.id.home) {
+            return super.onOptionsItemSelected(item);
+        }
+        finish();
+        return true;
     }
 }
