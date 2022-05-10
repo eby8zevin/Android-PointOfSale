3d4d412e8bf7c79004bc24b19478ec489a700cbc
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-05-10 15:21:24 +0700

    Customers Activity

---
+++ app/src/main/java/com/ahmadabuhasan/pointofsale/customers/CustomersActivity.java
@@ -0,0 +1,167 @@
+package com.ahmadabuhasan.pointofsale.customers;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.ahmadabuhasan.pointofsale.Constant;
+import com.ahmadabuhasan.pointofsale.R;
+import com.ahmadabuhasan.pointofsale.database.DatabaseAccess;
+import com.ahmadabuhasan.pointofsale.database.DatabaseOpenHelper;
+import com.ahmadabuhasan.pointofsale.databinding.ActivityCustomersBinding;
+import com.ahmadabuhasan.pointofsale.utils.BaseActivity;
+import com.ajts.androidmads.library.SQLiteToExcel;
+import com.obsez.android.lib.filechooser.ChooserDialog;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
+import es.dmoral.toasty.Toasty;
+
+public class CustomersActivity extends BaseActivity {
+
+    private ActivityCustomersBinding binding;
+    ProgressDialog loading;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityCustomersBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Objects.requireNonNull(getSupportActionBar()).setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.all_customer);
+
+        this.binding.customerRecyclerview.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        this.binding.customerRecyclerview.setHasFixedSize(true);
+
+        DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
+        databaseAccess.open();
+
+        List<HashMap<String, String>> customerData = databaseAccess.getCustomers();
+        Log.d("data", "" + customerData.size());
+        if (customerData.size() <= 0) {
+            Toasty.info(this, R.string.no_customer_found, Toasty.LENGTH_SHORT).show();
+            this.binding.ivNoCustomer.setImageResource(R.drawable.no_data);
+        } else {
+            this.binding.ivNoCustomer.setVisibility(View.GONE);
+            CustomerAdapter adapter = new CustomerAdapter(this, customerData);
+            this.binding.customerRecyclerview.setAdapter(adapter);
+        }
+
+        this.binding.etCustomerSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                DatabaseAccess databaseAccess1 = DatabaseAccess.getInstance(CustomersActivity.this);
+                databaseAccess1.open();
+
+                List<HashMap<String, String>> searchCustomerList = databaseAccess1.searchCustomers(charSequence.toString());
+                if (searchCustomerList.size() <= 0) {
+                    CustomersActivity.this.binding.customerRecyclerview.setVisibility(View.GONE);
+                    CustomersActivity.this.binding.ivNoCustomer.setVisibility(View.VISIBLE);
+                    CustomersActivity.this.binding.ivNoCustomer.setImageResource(R.drawable.no_data);
+                    return;
+                }
+                CustomersActivity.this.binding.ivNoCustomer.setVisibility(View.GONE);
+                CustomersActivity.this.binding.customerRecyclerview.setVisibility(View.VISIBLE);
+
+                CustomerAdapter adapter = new CustomerAdapter(CustomersActivity.this, searchCustomerList);
+                CustomersActivity.this.binding.customerRecyclerview.setAdapter(adapter);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        this.binding.fabAdd.setOnClickListener(view -> CustomersActivity.this.startActivity(new Intent(CustomersActivity.this, AddCustomersActivity.class)));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.add_customer_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_export_customer) {
+            folderChooser();
+            return true;
+        } else if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void folderChooser() {
+        new ChooserDialog((Activity) this)
+                .displayPath(true)
+                .withFilter(true, false, new String[0])
+                .withChosenListener(new ChooserDialog.Result() {
+                    @Override
+                    public void onChoosePath(String dir, File dirFile) {
+                        CustomersActivity.this.onExport(dir);
+                        Log.d("path", dir);
+                    }
+                }).build().show();
+    }
+
+    public void onExport(String path) {
+        File file = new File(path);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        SQLiteToExcel sqLiteToExcel = new SQLiteToExcel(getApplicationContext(), DatabaseOpenHelper.DATABASE_NAME, path);
+        sqLiteToExcel.exportSingleTable(Constant.customers, "customers.xls", new SQLiteToExcel.ExportListener() {
+            @Override
+            public void onStart() {
+                CustomersActivity.this.loading = new ProgressDialog(CustomersActivity.this);
+                CustomersActivity.this.loading.setMessage(CustomersActivity.this.getString(R.string.data_exporting_please_wait));
+                CustomersActivity.this.loading.setCancelable(false);
+                CustomersActivity.this.loading.show();
+            }
+
+            @Override
+            public void onCompleted(String filePath) {
+                Handler mHand = new Handler();
+                mHand.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        CustomersActivity.this.loading.dismiss();
+                        Toasty.success(CustomersActivity.this, R.string.data_successfully_exported, Toasty.LENGTH_SHORT).show();
+                    }
+                }, 5000L);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                CustomersActivity.this.loading.dismiss();
+                Toasty.error(CustomersActivity.this, R.string.data_export_fail, Toasty.LENGTH_SHORT).show();
+                Log.d("Error", e.toString());
+            }
+        });
+    }
+}
