145c9b8bfea3fbe92aee2bbf8fee0af1dd393625
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-05-11 14:01:46 +0700

    Edit Customers Activity

--- app/src/main/java/com/ahmadabuhasan/pointofsale/customers/EditCustomersActivity.java
+++ app/src/main/java/com/ahmadabuhasan/pointofsale/customers/EditCustomersActivity.java
@@ -59,39 +59,36 @@ public class EditCustomersActivity extends BaseActivity {
             EditCustomersActivity.this.binding.tvUpdateCustomer.setVisibility(View.VISIBLE);
         });
 
-        this.binding.tvUpdateCustomer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String customer_name = EditCustomersActivity.this.binding.etCustomerName.getText().toString().trim();
-                String customer_cell = EditCustomersActivity.this.binding.etCustomerCell.getText().toString().trim();
-                String customer_email = EditCustomersActivity.this.binding.etCustomerEmail.getText().toString().trim();
-                String customer_address = EditCustomersActivity.this.binding.etCustomerAddress.getText().toString().trim();
+        this.binding.tvUpdateCustomer.setOnClickListener(view -> {
+            String customer_name = EditCustomersActivity.this.binding.etCustomerName.getText().toString().trim();
+            String customer_cell = EditCustomersActivity.this.binding.etCustomerCell.getText().toString().trim();
+            String customer_email = EditCustomersActivity.this.binding.etCustomerEmail.getText().toString().trim();
+            String customer_address = EditCustomersActivity.this.binding.etCustomerAddress.getText().toString().trim();
 
-                if (customer_name.isEmpty()) {
-                    EditCustomersActivity.this.binding.etCustomerName.setError(EditCustomersActivity.this.getString(R.string.enter_customer_name));
-                    EditCustomersActivity.this.binding.etCustomerName.requestFocus();
-                } else if (customer_cell.isEmpty()) {
-                    EditCustomersActivity.this.binding.etCustomerCell.setError(EditCustomersActivity.this.getString(R.string.enter_customer_cell));
-                    EditCustomersActivity.this.binding.etCustomerCell.requestFocus();
-                } else if (customer_email.isEmpty() || !customer_email.contains("@") || !customer_email.contains(".")) {
-                    EditCustomersActivity.this.binding.etCustomerEmail.setError(EditCustomersActivity.this.getString(R.string.enter_valid_email));
-                    EditCustomersActivity.this.binding.etCustomerEmail.requestFocus();
-                } else if (customer_address.isEmpty()) {
-                    EditCustomersActivity.this.binding.etCustomerAddress.setError(EditCustomersActivity.this.getString(R.string.enter_customer_address));
-                    EditCustomersActivity.this.binding.etCustomerAddress.requestFocus();
-                } else {
-                    DatabaseAccess databaseAccess = DatabaseAccess.getInstance(EditCustomersActivity.this);
-                    databaseAccess.open();
+            if (customer_name.isEmpty()) {
+                EditCustomersActivity.this.binding.etCustomerName.setError(EditCustomersActivity.this.getString(R.string.enter_customer_name));
+                EditCustomersActivity.this.binding.etCustomerName.requestFocus();
+            } else if (customer_cell.isEmpty()) {
+                EditCustomersActivity.this.binding.etCustomerCell.setError(EditCustomersActivity.this.getString(R.string.enter_customer_cell));
+                EditCustomersActivity.this.binding.etCustomerCell.requestFocus();
+            } else if (customer_email.isEmpty() || !customer_email.contains("@") || !customer_email.contains(".")) {
+                EditCustomersActivity.this.binding.etCustomerEmail.setError(EditCustomersActivity.this.getString(R.string.enter_valid_email));
+                EditCustomersActivity.this.binding.etCustomerEmail.requestFocus();
+            } else if (customer_address.isEmpty()) {
+                EditCustomersActivity.this.binding.etCustomerAddress.setError(EditCustomersActivity.this.getString(R.string.enter_customer_address));
+                EditCustomersActivity.this.binding.etCustomerAddress.requestFocus();
+            } else {
+                DatabaseAccess databaseAccess = DatabaseAccess.getInstance(EditCustomersActivity.this);
+                databaseAccess.open();
 
-                    if (databaseAccess.updateCustomer(getCustomer_id, customer_name, customer_cell, customer_email, customer_address)) {
-                        Toasty.success(EditCustomersActivity.this, R.string.update_successfully, Toasty.LENGTH_SHORT).show();
-                        Intent i = new Intent(EditCustomersActivity.this, CustomersActivity.class);
-                        //i.addFlags(PagedChannelRandomAccessSource.DEFAULT_TOTAL_BUFSIZE;
-                        EditCustomersActivity.this.startActivity(i);
-                        return;
-                    }
-                    Toasty.error(EditCustomersActivity.this, R.string.failed, Toasty.LENGTH_SHORT).show();
+                if (databaseAccess.updateCustomer(getCustomer_id, customer_name, customer_cell, customer_email, customer_address)) {
+                    Toasty.success(EditCustomersActivity.this, R.string.update_successfully, Toasty.LENGTH_SHORT).show();
+                    Intent i = new Intent(EditCustomersActivity.this, CustomersActivity.class);
+                    //i.addFlags(PagedChannelRandomAccessSource.DEFAULT_TOTAL_BUFSIZE;
+                    EditCustomersActivity.this.startActivity(i);
+                    return;
                 }
+                Toasty.error(EditCustomersActivity.this, R.string.failed, Toasty.LENGTH_SHORT).show();
             }
         });
     }
