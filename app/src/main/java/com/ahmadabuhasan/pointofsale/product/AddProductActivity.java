3f2b4ac9d7a12af6daed68edf6715ed40f28e4e4
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-05-11 15:14:33 +0700

    add Upload Image

--- app/src/main/java/com/ahmadabuhasan/pointofsale/product/AddProductActivity.java
+++ app/src/main/java/com/ahmadabuhasan/pointofsale/product/AddProductActivity.java
@@ -1,21 +1,48 @@
 package com.ahmadabuhasan.pointofsale.product;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-
+import com.ahmadabuhasan.pointofsale.Constant;
 import com.ahmadabuhasan.pointofsale.R;
+import com.ahmadabuhasan.pointofsale.database.DatabaseAccess;
 import com.ahmadabuhasan.pointofsale.databinding.ActivityAddProductBinding;
 
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Objects;
+
+import in.mayanknagwanshi.imagepicker.ImageSelectActivity;
 
 public class AddProductActivity extends AppCompatActivity {
 
     public static EditText etProductCode;
     private ActivityAddProductBinding binding;
+
+    ArrayAdapter<String> categoryAdapter;
+    ArrayAdapter<String> weightUnitAdapter;
+    ArrayAdapter<String> supplierAdapter;
+
+    List<String> categoryNames;
+    List<String> weightUnitNames;
+    List<String> supplierNames;
+
+    String encodedImage = "N/A";
+    ProgressDialog loading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +54,99 @@ public class AddProductActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(R.string.add_product);
 
-        etProductCode = binding.etProductCode;
+        etProductCode = findViewById(R.id.et_product_code);
 
         this.binding.ivScanCode.setOnClickListener(view -> AddProductActivity.this.startActivity(new Intent(AddProductActivity.this, ScannerViewActivity.class)));
 
+        this.binding.tvChooseImage.setOnClickListener(view -> {
+            Intent i = new Intent(AddProductActivity.this, ImageSelectActivity.class);
+            i.putExtra(ImageSelectActivity.FLAG_COMPRESS, true);
+            i.putExtra(ImageSelectActivity.FLAG_CAMERA, true);
+            i.putExtra(ImageSelectActivity.FLAG_GALLERY, true);
+            AddProductActivity.this.startActivityForResult(i, 1213);
+        });
+
+        this.binding.ivProduct.setOnClickListener(view -> {
+            Intent i = new Intent(AddProductActivity.this, ImageSelectActivity.class);
+            i.putExtra(ImageSelectActivity.FLAG_COMPRESS, true);
+            i.putExtra(ImageSelectActivity.FLAG_CAMERA, true);
+            i.putExtra(ImageSelectActivity.FLAG_GALLERY, true);
+            AddProductActivity.this.startActivityForResult(i, 1213);
+        });
+
+        this.categoryNames = new ArrayList<>();
+        this.weightUnitNames = new ArrayList<>();
+        this.supplierNames = new ArrayList<>();
+
+        DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
+
+        databaseAccess.open();
+        final List<HashMap<String, String>> productCategory = databaseAccess.getProductCategory();
+        for (int i = 0; i < productCategory.size(); i++) {
+            this.categoryNames.add(productCategory.get(i).get(Constant.CATEGORY_NAME));
+        }
+
+        databaseAccess.open();
+        final List<HashMap<String, String>> weightUnit = databaseAccess.getWeightUnit();
+        for (int i1 = 0; i1 < weightUnit.size(); i1++) {
+            this.weightUnitNames.add(weightUnit.get(i1).get(Constant.WEIGHT_UNIT));
+        }
+
+        databaseAccess.open();
+        final List<HashMap<String, String>> productSupplier = databaseAccess.getProductSupplier();
+        for (int i2 = 0; i2 < productSupplier.size(); i2++) {
+            this.supplierNames.add(productSupplier.get(i2).get(Constant.SUPPLIERS_NAME));
+        }
+
+        this.binding.etProductCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        this.binding.etProductWeightUnit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        this.binding.etSupplier.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        this.binding.tvAddProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1213 && resultCode == Activity.RESULT_OK && data != null) {
+            try {
+                String filePath = data.getStringExtra(ImageSelectActivity.RESULT_FILE_PATH);
+                Bitmap selectedImage = BitmapFactory.decodeFile(filePath);
+                this.binding.ivProduct.setImageBitmap(selectedImage);
+                this.encodedImage = encodeImage(selectedImage);
+            } catch (Exception e) {
+                Toast.makeText(this, R.string.something_went_wrong, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private String encodeImage(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        String encImage = Base64.encodeToString(bytes, Base64.DEFAULT);
+        return encImage;
     }
 }
