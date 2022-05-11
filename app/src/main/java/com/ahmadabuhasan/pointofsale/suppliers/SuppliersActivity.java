15d1118d404cba47b3fdd0d566326843bf96ff88
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-05-11 08:24:31 +0700

    Suppliers Activity

--- app/src/main/java/com/ahmadabuhasan/pointofsale/suppliers/SuppliersActivity.java
+++ app/src/main/java/com/ahmadabuhasan/pointofsale/suppliers/SuppliersActivity.java
@@ -1,22 +1,40 @@
 package com.ahmadabuhasan.pointofsale.suppliers;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.ahmadabuhasan.pointofsale.Constant;
 import com.ahmadabuhasan.pointofsale.R;
 import com.ahmadabuhasan.pointofsale.database.DatabaseAccess;
+import com.ahmadabuhasan.pointofsale.database.DatabaseOpenHelper;
 import com.ahmadabuhasan.pointofsale.databinding.ActivitySuppliersBinding;
 import com.ahmadabuhasan.pointofsale.utils.BaseActivity;
+import com.ajts.androidmads.library.SQLiteToExcel;
+import com.obsez.android.lib.filechooser.ChooserDialog;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 
+import es.dmoral.toasty.Toasty;
+
 public class SuppliersActivity extends BaseActivity {
 
     private ActivitySuppliersBinding binding;
+    ProgressDialog loading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,5 +54,113 @@ public class SuppliersActivity extends BaseActivity {
 
         List<HashMap<String, String>> supplierData = databaseAccess.getSuppliers();
         Log.d("data", "" + supplierData.size());
+        if (supplierData.size() <= 0) {
+            Toasty.info(this, R.string.no_suppliers_found, Toasty.LENGTH_SHORT).show();
+            this.binding.ivNoSupplier.setImageResource(R.drawable.no_data);
+        } else {
+            this.binding.ivNoSupplier.setVisibility(View.GONE);
+            SupplierAdapter adapter = new SupplierAdapter(this, supplierData);
+            this.binding.supplierRecyclerview.setAdapter(adapter);
+        }
+
+        this.binding.etSupplierSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                DatabaseAccess databaseAccess1 = DatabaseAccess.getInstance(SuppliersActivity.this);
+                databaseAccess1.open();
+
+                List<HashMap<String, String>> searchSupplier = databaseAccess1.searchSuppliers(charSequence.toString());
+                if (searchSupplier.size() <= 0) {
+                    SuppliersActivity.this.binding.supplierRecyclerview.setVisibility(View.GONE);
+                    SuppliersActivity.this.binding.ivNoSupplier.setVisibility(View.VISIBLE);
+                    SuppliersActivity.this.binding.ivNoSupplier.setImageResource(R.drawable.no_data);
+                    return;
+                }
+                SuppliersActivity.this.binding.ivNoSupplier.setVisibility(View.GONE);
+                SuppliersActivity.this.binding.supplierRecyclerview.setVisibility(View.VISIBLE);
+
+                SupplierAdapter adapter1 = new SupplierAdapter(SuppliersActivity.this, searchSupplier);
+                SuppliersActivity.this.binding.supplierRecyclerview.setAdapter(adapter1);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        this.binding.fabAdd.setOnClickListener(view -> SuppliersActivity.this.startActivity(new Intent(SuppliersActivity.this, AddSuppliersActivity.class)));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.export_suppliers_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_export_supplier) {
+            folderChooser();
+            return true;
+        } else if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void folderChooser() {
+        new ChooserDialog((Activity) this)
+                .displayPath(true)
+                .withFilter(true, false, new String[0])
+                .withChosenListener(new ChooserDialog.Result() {
+                    @Override
+                    public void onChoosePath(String dir, File dirFile) {
+                        SuppliersActivity.this.onExport(dir);
+                        Log.d("path", dir);
+                    }
+                }).build().show();
+    }
+
+    public void onExport(String path) {
+        File file = new File(path);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        SQLiteToExcel sqLiteToExcel = new SQLiteToExcel(getApplicationContext(), DatabaseOpenHelper.DATABASE_NAME, path);
+        sqLiteToExcel.exportSingleTable(Constant.suppliers, "suppliers.xls", new SQLiteToExcel.ExportListener() {
+            @Override
+            public void onStart() {
+                SuppliersActivity.this.loading = new ProgressDialog(SuppliersActivity.this);
+                SuppliersActivity.this.loading.setMessage(SuppliersActivity.this.getString(R.string.data_exporting_please_wait));
+                SuppliersActivity.this.loading.setCancelable(false);
+                SuppliersActivity.this.loading.show();
+            }
+
+            @Override
+            public void onCompleted(String filePath) {
+                Handler mHand = new Handler();
+                mHand.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        SuppliersActivity.this.loading.dismiss();
+                        Toasty.success(SuppliersActivity.this, R.string.data_successfully_exported, Toasty.LENGTH_SHORT).show();
+                    }
+                }, 5000L);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                SuppliersActivity.this.loading.dismiss();
+                Toasty.error(SuppliersActivity.this, R.string.data_export_fail, Toasty.LENGTH_SHORT).show();
+            }
+        });
     }
 }
