0520b21f4cfc2b64fe419270ea6b4e8ba90a8687
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-05-12 10:10:28 +0700

    add Function "getProductsInfo"

--- app/src/main/java/com/ahmadabuhasan/pointofsale/database/DatabaseAccess.java
+++ app/src/main/java/com/ahmadabuhasan/pointofsale/database/DatabaseAccess.java
@@ -1121,54 +1121,54 @@ public class DatabaseAccess {
         return product;
     }
 
-    /*
-        public ArrayList<HashMap<String, String>> getProductsInfo(String product_id) {
-            ArrayList<HashMap<String, String>> product = new ArrayList<>();
-            SQLiteDatabase sQLiteDatabase = this.database;
-            Cursor cursor = sQLiteDatabase.rawQuery("SELECT * FROM products WHERE product_id='" + product_id + "'", null);
-            if (cursor.moveToFirst()) {
-                do {
-                    HashMap<String, String> map = new HashMap<>();
-                    map.put(Constant.PRODUCT_ID, cursor.getString(0));
-                    map.put(Constant.PRODUCT_NAME, cursor.getString(1));
-                    map.put(Constant.PRODUCT_CODE, cursor.getString(2));
-                    map.put(Constant.PRODUCT_CATEGORY, cursor.getString(3));
-                    map.put(Constant.PRODUCT_DESCRIPTION, cursor.getString(4));
-                    map.put("product_buy_price", cursor.getString(5));
-                    map.put(Constant.PRODUCT_SELL_PRICE, cursor.getString(6));
-                    map.put(Constant.PRODUCT_SUPPLIER, cursor.getString(7));
-                    map.put(Constant.PRODUCT_IMAGE, cursor.getString(8));
-                    map.put("product_stock", cursor.getString(9));
-                    map.put(Constant.PRODUCT_WEIGHT_UNIT_ID, cursor.getString(10));
-                    map.put(Constant.PRODUCT_WEIGHT, cursor.getString(11));
-                    product.add(map);
-                } while (cursor.moveToNext());
-            }
-            cursor.close();
-            this.database.close();
-            return product;
-        }
+    public ArrayList<HashMap<String, String>> getProductsInfo(String product_id) {
+        ArrayList<HashMap<String, String>> product = new ArrayList<>();
+        Cursor cursor = this.database.rawQuery("SELECT * FROM products WHERE product_id='" + product_id + "'", null);
+        if (cursor.moveToFirst()) {
+            do {
+                HashMap<String, String> map = new HashMap<>();
+                map.put(Constant.PRODUCT_ID, cursor.getString(0));
+                map.put(Constant.PRODUCT_NAME, cursor.getString(1));
+                map.put(Constant.PRODUCT_CODE, cursor.getString(2));
+                map.put(Constant.PRODUCT_CATEGORY, cursor.getString(3));
+                map.put(Constant.PRODUCT_DESCRIPTION, cursor.getString(4));
+                map.put(Constant.PRODUCT_BUY_PRICE, cursor.getString(5));
+                map.put(Constant.PRODUCT_SELL_PRICE, cursor.getString(6));
+                map.put(Constant.PRODUCT_SUPPLIER, cursor.getString(7));
+                map.put(Constant.PRODUCT_IMAGE, cursor.getString(8));
+                map.put(Constant.PRODUCT_STOCK, cursor.getString(9));
+                map.put(Constant.PRODUCT_WEIGHT_UNIT_ID, cursor.getString(10));
+                map.put(Constant.PRODUCT_WEIGHT, cursor.getString(11));
 
-        public ArrayList<HashMap<String, String>> getAllExpense() {
-            ArrayList<HashMap<String, String>> product = new ArrayList<>();
-            Cursor cursor = this.database.rawQuery("SELECT * FROM expense ORDER BY expense_id DESC", null);
-            if (cursor.moveToFirst()) {
-                do {
-                    HashMap<String, String> map = new HashMap<>();
-                    map.put(Constant.EXPENSE_ID, cursor.getString(cursor.getColumnIndex(Constant.EXPENSE_ID)));
-                    map.put(Constant.EXPENSE_NAME, cursor.getString(cursor.getColumnIndex(Constant.EXPENSE_NAME)));
-                    map.put(Constant.EXPENSE_NOTE, cursor.getString(cursor.getColumnIndex(Constant.EXPENSE_NOTE)));
-                    map.put(Constant.EXPENSE_AMOUNT, cursor.getString(cursor.getColumnIndex(Constant.EXPENSE_AMOUNT)));
-                    map.put(Constant.EXPENSE_DATE, cursor.getString(cursor.getColumnIndex(Constant.EXPENSE_DATE)));
-                    map.put(Constant.EXPENSE_TIME, cursor.getString(cursor.getColumnIndex(Constant.EXPENSE_TIME)));
-                    product.add(map);
-                } while (cursor.moveToNext());
-            }
-            cursor.close();
-            this.database.close();
-            return product;
+                product.add(map);
+            } while (cursor.moveToNext());
         }
-*/
+        cursor.close();
+        close();
+        return product;
+    }
+
+    /*
+            public ArrayList<HashMap<String, String>> getAllExpense() {
+                ArrayList<HashMap<String, String>> product = new ArrayList<>();
+                Cursor cursor = this.database.rawQuery("SELECT * FROM expense ORDER BY expense_id DESC", null);
+                if (cursor.moveToFirst()) {
+                    do {
+                        HashMap<String, String> map = new HashMap<>();
+                        map.put(Constant.EXPENSE_ID, cursor.getString(cursor.getColumnIndex(Constant.EXPENSE_ID)));
+                        map.put(Constant.EXPENSE_NAME, cursor.getString(cursor.getColumnIndex(Constant.EXPENSE_NAME)));
+                        map.put(Constant.EXPENSE_NOTE, cursor.getString(cursor.getColumnIndex(Constant.EXPENSE_NOTE)));
+                        map.put(Constant.EXPENSE_AMOUNT, cursor.getString(cursor.getColumnIndex(Constant.EXPENSE_AMOUNT)));
+                        map.put(Constant.EXPENSE_DATE, cursor.getString(cursor.getColumnIndex(Constant.EXPENSE_DATE)));
+                        map.put(Constant.EXPENSE_TIME, cursor.getString(cursor.getColumnIndex(Constant.EXPENSE_TIME)));
+                        product.add(map);
+                    } while (cursor.moveToNext());
+                }
+                cursor.close();
+                this.database.close();
+                return product;
+            }
+    */
     public ArrayList<HashMap<String, String>> getProductCategory() {
         ArrayList<HashMap<String, String>> product_category = new ArrayList<>();
         Cursor cursor = this.database.rawQuery("SELECT * FROM product_category ORDER BY category_id DESC", null);
