1628a169e94bbf37a8b5413c381dcbb46f3c50e9
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-05-14 13:58:33 +0700

    add Function "getTotalTaxForGraph"

--- app/src/main/java/com/ahmadabuhasan/pointofsale/database/DatabaseAccess.java
+++ app/src/main/java/com/ahmadabuhasan/pointofsale/database/DatabaseAccess.java
@@ -790,65 +790,65 @@ public class DatabaseAccess {
             this.database.close();
             return total_tax;
         }
-
-        public double getTotalTaxForGraph(String type, int currentYear) {
-            Cursor cursor;
-            double total_tax = Utils.DOUBLE_EPSILON;
-            if (type.equals(Constant.MONTHLY)) {
-                String currentMonth = new SimpleDateFormat("MM", Locale.ENGLISH).format(new Date());
-                cursor = this.database.rawQuery("SELECT * FROM order_list WHERE order_status='Completed'  AND  strftime('%m', order_date) = '" + currentMonth + "' ", null);
-            } else if (type.equals(Constant.YEARLY)) {
-                cursor = this.database.rawQuery("SELECT * FROM order_list WHERE order_status='Completed'  AND strftime('%Y', order_date) = '" + currentYear + "' ", null);
-            } else if (type.equals(Constant.DAILY)) {
-                String currentDate = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH).format(new Date());
-                SQLiteDatabase sQLiteDatabase = this.database;
-                cursor = sQLiteDatabase.rawQuery("SELECT * FROM order_list WHERE order_status='Completed'  AND   order_date='" + currentDate + "' ORDER BY order_id DESC", null);
-            } else {
-                cursor = this.database.rawQuery("SELECT * FROM order_list WHERE order_status='Completed' ", null);
-            }
-            if (cursor.moveToFirst()) {
-                do {
-                    total_tax += Double.parseDouble(cursor.getString(cursor.getColumnIndex(Constant.TAX)));
-                } while (cursor.moveToNext());
-            } else {
-                total_tax = Utils.DOUBLE_EPSILON;
-            }
-            cursor.close();
-            this.database.close();
-            return total_tax;
-        }
-
-        public double getTotalOrderPrice(String type) {
-            Cursor cursor;
-            double total_price = Utils.DOUBLE_EPSILON;
-            if (type.equals(Constant.MONTHLY)) {
-                String currentMonth = new SimpleDateFormat("MM", Locale.ENGLISH).format(new Date());
-                cursor = this.database.rawQuery("SELECT * FROM order_details WHERE order_status='Completed'  AND strftime('%m', product_order_date) = '" + currentMonth + "' ", null);
-            } else if (type.equals(Constant.YEARLY)) {
-                String currentYear = new SimpleDateFormat("yyyy", Locale.ENGLISH).format(new Date());
-                cursor = this.database.rawQuery("SELECT * FROM order_details WHERE order_status='Completed'  AND  strftime('%Y', product_order_date) = '" + currentYear + "' ", null);
-            } else if (type.equals(Constant.DAILY)) {
-                String currentDate = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH).format(new Date());
-                SQLiteDatabase sQLiteDatabase = this.database;
-                cursor = sQLiteDatabase.rawQuery("SELECT * FROM order_details WHERE order_status='Completed'  AND   product_order_date='" + currentDate + "' ORDER BY order_Details_id DESC", null);
-            } else {
-                cursor = this.database.rawQuery("SELECT * FROM order_details WHERE order_status='Completed' ", null);
-            }
-            if (cursor.moveToFirst()) {
-                do {
-                    double price = Double.parseDouble(cursor.getString(5));
-                    double parseInt = (double) Integer.parseInt(cursor.getString(4));
-                    Double.isNaN(parseInt);
-                    total_price += parseInt * price;
-                } while (cursor.moveToNext());
-            } else {
-                total_price = Utils.DOUBLE_EPSILON;
-            }
-            cursor.close();
-            this.database.close();
-            return total_price;
-        }
 */
+    public double getTotalTaxForGraph(String type, int currentYear) {
+        Cursor cursor;
+        double total_tax = Utils.DOUBLE_EPSILON;
+        if (type.equals(Constant.MONTHLY)) {
+            String currentMonth = new SimpleDateFormat("MM", Locale.ENGLISH).format(new Date());
+            cursor = this.database.rawQuery("SELECT * FROM order_list WHERE order_status='Completed'  AND  strftime('%m', order_date) = '" + currentMonth + "' ", null);
+        } else if (type.equals(Constant.YEARLY)) {
+            cursor = this.database.rawQuery("SELECT * FROM order_list WHERE order_status='Completed'  AND strftime('%Y', order_date) = '" + currentYear + "' ", null);
+        } else if (type.equals(Constant.DAILY)) {
+            String currentDate = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH).format(new Date());
+            cursor = this.database.rawQuery("SELECT * FROM order_list WHERE order_status='Completed'  AND   order_date='" + currentDate + "' ORDER BY order_id DESC", null);
+        } else {
+            cursor = this.database.rawQuery("SELECT * FROM order_list WHERE order_status='Completed' ", null);
+        }
+        if (cursor.moveToFirst()) {
+            do {
+                total_tax += Double.parseDouble(cursor.getString(7));
+            } while (cursor.moveToNext());
+        } else {
+            total_tax = Utils.DOUBLE_EPSILON;
+        }
+        cursor.close();
+        close();
+        return total_tax;
+    }
+
+    /*
+            public double getTotalOrderPrice(String type) {
+                Cursor cursor;
+                double total_price = Utils.DOUBLE_EPSILON;
+                if (type.equals(Constant.MONTHLY)) {
+                    String currentMonth = new SimpleDateFormat("MM", Locale.ENGLISH).format(new Date());
+                    cursor = this.database.rawQuery("SELECT * FROM order_details WHERE order_status='Completed'  AND strftime('%m', product_order_date) = '" + currentMonth + "' ", null);
+                } else if (type.equals(Constant.YEARLY)) {
+                    String currentYear = new SimpleDateFormat("yyyy", Locale.ENGLISH).format(new Date());
+                    cursor = this.database.rawQuery("SELECT * FROM order_details WHERE order_status='Completed'  AND  strftime('%Y', product_order_date) = '" + currentYear + "' ", null);
+                } else if (type.equals(Constant.DAILY)) {
+                    String currentDate = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH).format(new Date());
+                    SQLiteDatabase sQLiteDatabase = this.database;
+                    cursor = sQLiteDatabase.rawQuery("SELECT * FROM order_details WHERE order_status='Completed'  AND   product_order_date='" + currentDate + "' ORDER BY order_Details_id DESC", null);
+                } else {
+                    cursor = this.database.rawQuery("SELECT * FROM order_details WHERE order_status='Completed' ", null);
+                }
+                if (cursor.moveToFirst()) {
+                    do {
+                        double price = Double.parseDouble(cursor.getString(5));
+                        double parseInt = (double) Integer.parseInt(cursor.getString(4));
+                        Double.isNaN(parseInt);
+                        total_price += parseInt * price;
+                    } while (cursor.moveToNext());
+                } else {
+                    total_price = Utils.DOUBLE_EPSILON;
+                }
+                cursor.close();
+                this.database.close();
+                return total_price;
+            }
+    */
     public double getTotalOrderPriceForGraph(String type, int currentYear) {
         Cursor cursor;
         double total_price = Utils.DOUBLE_EPSILON;
