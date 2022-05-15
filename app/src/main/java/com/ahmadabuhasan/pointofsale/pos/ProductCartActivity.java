a99623a76899153b528504939bf1389b2950ae67
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-05-15 15:34:53 +0700

    Product Cart Activity

---
+++ app/src/main/java/com/ahmadabuhasan/pointofsale/pos/ProductCartActivity.java
@@ -0,0 +1,15 @@
+package com.ahmadabuhasan.pointofsale.pos;
+
+import android.os.Bundle;
+
+import com.ahmadabuhasan.pointofsale.R;
+import com.ahmadabuhasan.pointofsale.utils.BaseActivity;
+
+public class ProductCartActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_cart);
+    }
+}
