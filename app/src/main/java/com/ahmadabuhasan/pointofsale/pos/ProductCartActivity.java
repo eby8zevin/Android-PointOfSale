1e26bb282c8a6fca321210ab2f18bc807f84ab82
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-05-17 13:06:05 +0700

    ProductCart Activity

--- app/src/main/java/com/ahmadabuhasan/pointofsale/pos/ProductCartActivity.java
+++ app/src/main/java/com/ahmadabuhasan/pointofsale/pos/ProductCartActivity.java
@@ -1,18 +1,32 @@
 package com.ahmadabuhasan.pointofsale.pos;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.ahmadabuhasan.pointofsale.Constant;
 import com.ahmadabuhasan.pointofsale.R;
 import com.ahmadabuhasan.pointofsale.database.DatabaseAccess;
 import com.ahmadabuhasan.pointofsale.databinding.ActivityProductCartBinding;
 import com.ahmadabuhasan.pointofsale.utils.BaseActivity;
+import com.github.mikephil.charting.utils.Utils;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
@@ -24,6 +38,14 @@ import java.util.Objects;
 public class ProductCartActivity extends BaseActivity {
 
     private ActivityProductCartBinding binding;
+
+    DecimalFormat decimalFormat = new DecimalFormat("#0.00");
+    ArrayAdapter<String> customerAdapter;
+    List<String> customerNames;
+    ArrayAdapter<String> orderTypeAdapter;
+    List<String> orderTypeNames;
+    ArrayAdapter<String> paymentMethodAdapter;
+    List<String> paymentMethodNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +84,244 @@ public class ProductCartActivity extends BaseActivity {
     }
 
     public void dialog() {
+        DatabaseAccess databaseAccess1 = DatabaseAccess.getInstance(this);
+        databaseAccess1.open();
+
+        List<HashMap<String, String>> shopData = databaseAccess1.getShopInformation();
+        final String shop_currency = shopData.get(0).get(Constant.SHOP_CURRENCY);
+        String tax = shopData.get(0).get(Constant.TAX);
+        double getTax = Double.parseDouble(tax);
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        View dialogView = getLayoutInflater().inflate(R.layout.dialog_payment, (ViewGroup) null);
+        dialog.setView(dialogView);
+        dialog.setCancelable(false);
+
+        ImageButton ibClose = dialogView.findViewById(R.id.ib_close);
+        final Button btnSubmit = dialogView.findViewById(R.id.btn_submit);
+        final TextView tvDialogCustomer = dialogView.findViewById(R.id.tv_dialog_customer);
+        ImageButton ibCustomer = dialogView.findViewById(R.id.ib_dialog_customer);
+        final TextView tvDialogOrderType = dialogView.findViewById(R.id.tv_dialog_order_type);
+        ImageButton ibOrderType = dialogView.findViewById(R.id.ib_dialog_order_type);
+        final TextView tvDialogPaymentMethod = dialogView.findViewById(R.id.tv_dialog_order_status);
+        ImageButton ibPaymentMethod = dialogView.findViewById(R.id.ib_dialog_payment_method);
+        TextView tvDialogTotal = dialogView.findViewById(R.id.tv_dialog_total);
+        TextView tvDialogTax = dialogView.findViewById(R.id.tv_dialog_tax);
+        TextView tvTotalTax = dialogView.findViewById(R.id.tv_dialog_total_tax);
+        final EditText etDialogDiscount = dialogView.findViewById(R.id.et_dialog_discount);
+        final TextView tvDialogTotalCost = dialogView.findViewById(R.id.tv_dialog_total_cost);
+
+        final double total_cost = CartAdapter.total_price.doubleValue();
+        StringBuilder sb = new StringBuilder();
+        sb.append(shop_currency);
+        sb.append(this.decimalFormat.format(total_cost));
+        tvDialogTotal.setText(sb.toString());
+        tvDialogTax.setText(String.format("%s( %s%%) : ", getString(R.string.tax), tax));
+        final double calculated_tax = (total_cost * getTax) / 100.d;
+        tvTotalTax.setText(String.format("%s%s", shop_currency, this.decimalFormat.format(calculated_tax)));
+        double calculated_totalCost = (total_cost + calculated_tax) - Utils.DOUBLE_EPSILON;
+        tvDialogTotalCost.setText(String.format("%s%s", shop_currency, this.decimalFormat.format(calculated_totalCost)));
+
+        etDialogDiscount.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String getDiscount = charSequence.toString();
+                double discount = Double.parseDouble(getDiscount);
+                if (getDiscount.isEmpty() || getDiscount.equals(".")) {
+                    double calculated = (total_cost + calculated_tax) - Utils.DOUBLE_EPSILON;
+                    tvDialogTotalCost.setText(String.format("%s%s", shop_currency, decimalFormat.format(calculated)));
+                } else if (discount > calculated_totalCost) {
+                    etDialogDiscount.setError(getString(R.string.discount_cant_be_greater_than_total_price));
+                    etDialogDiscount.requestFocus();
+                    btnSubmit.setVisibility(View.INVISIBLE);
+                } else {
+                    btnSubmit.setVisibility(View.VISIBLE);
+                    double calculated_withDiscount = (total_cost + calculated_tax) - discount;
+                    tvDialogTotalCost.setText(String.format("%s%s", shop_currency, decimalFormat.format(calculated_withDiscount)));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        this.customerNames = new ArrayList<>();
+        databaseAccess1.open();
+        List<HashMap<String, String>> customer = databaseAccess1.getCustomers();
+        for (int i = 0; i < customer.size(); i++) {
+            this.customerNames.add(customer.get(i).get(Constant.CUSTOMER_NAME));
+        }
+
+        this.orderTypeNames = new ArrayList<>();
+        databaseAccess1.open();
+        List<HashMap<String, String>> order_type = databaseAccess1.getOrderType();
+        for (int i1 = 0; i1 < order_type.size(); i1++) {
+            this.orderTypeNames.add(order_type.get(i1).get(Constant.ORDER_TYPE_NAME));
+        }
+
+        this.paymentMethodNames = new ArrayList<>();
+        databaseAccess1.open();
+        List<HashMap<String, String>> payment_method = databaseAccess1.getPaymentMethod();
+        for (int i2 = 0; i2 < payment_method.size(); i2++) {
+            this.paymentMethodNames.add(payment_method.get(i2).get(Constant.PAYMENT_METHOD_NAME));
+        }
+
+        ibCustomer.setOnClickListener(view -> {
+            customerAdapter = new ArrayAdapter<>(ProductCartActivity.this, android.R.layout.simple_list_item_1);
+            customerAdapter.addAll(customerNames);
+
+            AlertDialog.Builder dialog1 = new AlertDialog.Builder(ProductCartActivity.this);
+            View dialogView1 = getLayoutInflater().inflate(R.layout.dialog_list_search, (ViewGroup) null);
+            dialog1.setView(dialogView1);
+            dialog1.setCancelable(false);
+
+            TextView title = dialogView1.findViewById(R.id.tv_dialog_title);
+            EditText search = dialogView1.findViewById(R.id.et_dialog_search);
+            ListView dialogListView = dialogView1.findViewById(R.id.dialog_listView);
+            Button btnCancel = dialogView1.findViewById(R.id.btn_dialog_cancel);
+
+            title.setText(R.string.select_customer);
+            dialogListView.setVerticalScrollBarEnabled(true);
+            dialogListView.setAdapter(customerAdapter);
+            search.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+                    customerAdapter.getFilter().filter(charSequence);
+                }
+
+                @Override
+                public void afterTextChanged(Editable editable) {
+
+                }
+            });
+            final AlertDialog alertDialog = dialog1.create();
+            btnCancel.setOnClickListener(view1 -> alertDialog.dismiss());
+            alertDialog.show();
+            dialogListView.setOnItemClickListener((adapterView, view12, position, l) -> {
+                alertDialog.dismiss();
+                String selectedItem = customerAdapter.getItem(position);
+                tvDialogCustomer.setText(selectedItem);
+            });
+        });
+
+        ibOrderType.setOnClickListener(view -> {
+            orderTypeAdapter = new ArrayAdapter<>(ProductCartActivity.this, android.R.layout.simple_list_item_1);
+            orderTypeAdapter.addAll(orderTypeNames);
+
+            AlertDialog.Builder dialog2 = new AlertDialog.Builder(ProductCartActivity.this);
+            View dialogView2 = getLayoutInflater().inflate(R.layout.dialog_list_search, (ViewGroup) null);
+            dialog2.setView(dialogView2);
+            dialog2.setCancelable(false);
+
+            TextView title = dialogView2.findViewById(R.id.tv_dialog_title);
+            EditText search = dialogView2.findViewById(R.id.et_dialog_search);
+            ListView dialogListView = dialogView2.findViewById(R.id.dialog_listView);
+            Button btnCancel = dialogView2.findViewById(R.id.btn_dialog_cancel);
+
+            title.setText(R.string.select_order_type);
+            dialogListView.setVerticalScrollBarEnabled(true);
+            dialogListView.setAdapter(orderTypeAdapter);
+            search.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+                    orderTypeAdapter.getFilter().filter(charSequence);
+                }
+
+                @Override
+                public void afterTextChanged(Editable editable) {
+
+                }
+            });
+            final AlertDialog alertDialog = dialog2.create();
+            btnCancel.setOnClickListener(view1 -> alertDialog.dismiss());
+            alertDialog.show();
+            dialogListView.setOnItemClickListener((adapterView, view12, position, l) -> {
+                alertDialog.dismiss();
+                String selectedItem = orderTypeAdapter.getItem(position);
+                tvDialogOrderType.setText(selectedItem);
+            });
+        });
+
+        ibPaymentMethod.setOnClickListener(view -> {
+            paymentMethodAdapter = new ArrayAdapter<>(ProductCartActivity.this, android.R.layout.simple_list_item_1);
+            paymentMethodAdapter.addAll(paymentMethodNames);
+
+            AlertDialog.Builder dialog3 = new AlertDialog.Builder(ProductCartActivity.this);
+            View dialogView3 = getLayoutInflater().inflate(R.layout.dialog_list_search, (ViewGroup) null);
+            dialog3.setView(dialogView3);
+            dialog3.setCancelable(false);
+
+            TextView title = dialogView3.findViewById(R.id.tv_dialog_title);
+            EditText search = dialogView3.findViewById(R.id.et_dialog_search);
+            ListView dialogListView = dialogView3.findViewById(R.id.dialog_listView);
+            Button btnCancel = dialogView3.findViewById(R.id.btn_dialog_cancel);
+
+            title.setText(R.string.select_payment_method);
+            dialogListView.setVerticalScrollBarEnabled(true);
+            dialogListView.setAdapter(paymentMethodAdapter);
+            search.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+                    paymentMethodAdapter.getFilter().filter(charSequence);
+                }
+
+                @Override
+                public void afterTextChanged(Editable editable) {
+
+                }
+            });
+            final AlertDialog alertDialog = dialog3.create();
+            btnCancel.setOnClickListener(view1 -> alertDialog.dismiss());
+            alertDialog.show();
+            dialogListView.setOnItemClickListener((adapterView, view12, position, l) -> {
+                alertDialog.dismiss();
+                String selectedItem = paymentMethodAdapter.getItem(position);
+                tvDialogPaymentMethod.setText(selectedItem);
+            });
+        });
+
+        final AlertDialog closeSubmit = dialog.create();
+        ibClose.setOnClickListener(view -> closeSubmit.dismiss());
+
+        btnSubmit.setOnClickListener(view -> {
+            String discount;
+            String customer_name = tvDialogCustomer.getText().toString().trim();
+            String order_type1 = tvDialogOrderType.getText().toString().trim();
+            String payment_method1 = tvDialogPaymentMethod.getText().toString().trim();
+            String disc = etDialogDiscount.getText().toString().trim();
+            if (disc.isEmpty()) {
+                discount = "0.00";
+            } else {
+                discount = disc;
+            }
+            proceedOrder(customer_name, order_type1, payment_method1, calculated_tax, discount);
+            closeSubmit.dismiss();
+        });
+    }
+
+    public void proceedOrder(String customer, String orderType, String paymentMethod, double tax, String discount) {
 
     }
 
