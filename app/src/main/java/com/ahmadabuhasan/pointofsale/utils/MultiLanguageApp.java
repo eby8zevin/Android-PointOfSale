8ebf23fef6fcc21cf9adc6f7192f8ec90669afa4
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-05-21 05:56:18 +0700

    MultiLanguageApp

--- app/src/main/java/com/ahmadabuhasan/pointofsale/utils/MultiLanguageApp.java
+++ app/src/main/java/com/ahmadabuhasan/pointofsale/utils/MultiLanguageApp.java
@@ -4,6 +4,10 @@ import android.app.Application;
 import android.content.Context;
 import android.content.res.Configuration;
 
+/*
+ * Created by Ahmad Abu Hasan (C) 2022
+ */
+
 public class MultiLanguageApp extends Application {
 
     @Override
