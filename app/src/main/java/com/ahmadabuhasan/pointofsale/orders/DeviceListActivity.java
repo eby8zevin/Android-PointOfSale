4600a28256ee22dd3d5363b4b1e18f741f7c6500
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-05-22 12:40:42 +0700

    DeviceList Activity

---
+++ app/src/main/java/com/ahmadabuhasan/pointofsale/orders/DeviceListActivity.java
@@ -0,0 +1,19 @@
+package com.ahmadabuhasan.pointofsale.orders;
+
+import android.os.Bundle;
+
+import com.ahmadabuhasan.pointofsale.R;
+import com.ahmadabuhasan.pointofsale.utils.BaseActivity;
+
+/*
+ * Created by Ahmad Abu Hasan (C) 2022
+ */
+
+public class DeviceListActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_list);
+    }
+}
