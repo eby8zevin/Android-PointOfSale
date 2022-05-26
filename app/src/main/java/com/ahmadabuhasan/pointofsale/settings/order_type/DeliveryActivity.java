db1f1c73b3c4b5e5faea1ce26b25fcde8c798e3a
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2022-05-26 18:49:34 +0700

    Delivery Activity

--- app/src/main/java/com/ahmadabuhasan/pointofsale/settings/order_type/DeliveryActivity.java
+++ app/src/main/java/com/ahmadabuhasan/pointofsale/settings/order_type/DeliveryActivity.java
@@ -29,7 +29,7 @@ import es.dmoral.toasty.Toasty;
 public class DeliveryActivity extends BaseActivity {
 
     private ActivityDeliveryBinding binding;
-    DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
+    DatabaseAccess databaseAccess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +44,7 @@ public class DeliveryActivity extends BaseActivity {
         this.binding.deliveryRecyclerview.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         this.binding.deliveryRecyclerview.setHasFixedSize(true);
 
+        databaseAccess = DatabaseAccess.getInstance(this);
         databaseAccess.open();
         List<HashMap<String, String>> deliveryData = databaseAccess.getOrderType();
         Log.d("data", "" + deliveryData.size());
